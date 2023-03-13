fb0d2f4a82b7b217fded1f818cc527fd939482e1
depdsouza <depdsouza@deloitte.com>
2023-03-13 19:06:37 +0530

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,14 +1,18 @@
-import { LightningElement, api} from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class StudentTile extends LightningElement {
     @api student = {
         Name: 'Dephney Dsouza',
         PhotoUrl: '/services/images/photo/003B0FakePictId',
-        };
+    };
 
     @api isSelected = false;
-    
+
     get tileSelected() {
         return this.isSelected ? "tile selected" : "tile";
-        }
+    }
+
+    studentClick() {
+        alert(this.student.Name);
+    }
 }
