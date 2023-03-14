8a62bec32a94a5c2513d0092476d82e4bd2b5a1d
depdsouza <depdsouza@deloitte.com>
2023-03-14 11:32:26 +0530

    2-7A Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,19 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire} from 'lwc';
+
+import getStudents from
+'@salesforce/apex/StudentBrowser.getStudents';
+
 
 export default class StudentBrowser extends LightningElement {
-    studentList = [];
+  @wire(getStudents) students;
+  @wire(getStudents, { instructorId: "", courseDeliveryId:
+""})
+students;
+    // studentList = [];
 
-    constructor() {
-        super();
-        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
-        this.studentList = studentNames.map( (studentName, index) => {
-          return {
-            'sobjectType': 'Contact',
-            'Name': studentName,
-            'PhotoUrl': '/services/images/photo/003B0FakePictId',
-            'Id': index
-          };
-        });
-      }
+    // constructor() {
+    //     super();
+    //     const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
+    //     this.studentList = studentNames.map( (studentName, index) => {
+    //       return {
+    //         'sobjectType': 'Contact',
+    //         'Name': studentName,
+    //         'PhotoUrl': '/services/images/photo/003B0FakePictId',
+    //         'Id': index
+    //       };
+    //     });
+    //   }
       
 }
