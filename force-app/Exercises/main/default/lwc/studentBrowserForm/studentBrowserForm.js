deb4fedad5ea42c65000e13ec63a936037633375
depdsouza <depdsouza@deloitte.com>
2023-03-14 16:45:23 +0530

    Exercise 2-9 Completed

--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -4,7 +4,12 @@ import getInstructors from
     '@salesforce/apex/StudentBrowserForm.getInstructors';
 import getDeliveriesByInstructor from '@salesforce/apex/StudentBrowserForm.getDeliveriesByInstructor'
     ;
-export default class StudentBrowserForm extends LightningElement {
+    import { NavigationMixin } from 'lightning/navigation';
+    import { encodeDefaultFieldValues } from
+    'lightning/pageReferenceUtils';
+
+
+export default class StudentBrowserForm extends NavigationMixin(LightningElement) {
     instructors = [];
     Error;
 
@@ -12,6 +17,7 @@ export default class StudentBrowserForm extends LightningElement {
     deliveries = [];
 
     selectedDeliveryId = '';
+    isButtonDisabled = true;
 
     @wire(getInstructors)
     wired_getInstructors({ error, data }) {
@@ -56,6 +62,8 @@ export default class StudentBrowserForm extends LightningElement {
     onInstructorChange(event) {
         this.selectedDeliveryId = '';
         this.selectedInstructorId = event.target.value;
+        this.isButtonDisabled = (this.selectedInstructorId ===
+            '');
         this.notifyParent();
     }
     onDeliveryChange(event) {
@@ -71,6 +79,25 @@ export default class StudentBrowserForm extends LightningElement {
         });
         this.dispatchEvent(evt);
     }
+
+    onAddNewDelivery() {
+        // Opens the new Course Delivery record modal dialog
+        // with the selected InstructorId prepopulated
+        const pageInfo = {
+            type: "standard__objectPage",
+            attributes: {
+                objectApiName: "Course_Delivery__c",
+                actionName: "new"
+            },
+            state: {
+                defaultFieldValues: encodeDefaultFieldValues({
+                    Instructor__c: this.selectedInstructorId
+                })
+            }
+        };
+        this[NavigationMixin.Navigate](pageInfo);
+    }
+
 
 
 }
