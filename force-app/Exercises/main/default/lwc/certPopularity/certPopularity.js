de90243d1b3b8dfbe9c910cecf8ef27e5a1bb625
depdsouza <depdsouza@deloitte.com>
2023-03-16 17:22:17 +0530

    Exercise 5-6 Completed

--- force-app/Exercises/main/default/lwc/certPopularity/certPopularity.js
+++ force-app/Exercises/main/default/lwc/certPopularity/certPopularity.js
@@ -1,5 +1,4 @@
 import { LightningElement } from 'lwc';
-
 import getCertPopularity from
     '@salesforce/apex/CertPopularity.getCertPopularity';
 import { loadScript } from
@@ -23,7 +22,6 @@ export default class CertPopularity extends LightningElement {
     error;
     _chart;
     _chartjsInitialized = false;
-
     renderedCallback() {
         if (this._chartjsInitialized) {
             return;
@@ -32,8 +30,31 @@ export default class CertPopularity extends LightningElement {
         loadScript(this, chartjs)
             .then(getCertPopularity)
             .then((result) => {
-                console.log('Data returned from Apex', result);
-                //TODO: process data and render chart here
+                console.log('Data returned from Apex',
+                    result);
+                //TODO: process data and render chart
+                let certData = result;
+                let certLabels = [];
+                let certCounts = [];
+                for (let i = 0; i < certData.length; i++) {
+                    certLabels.push(certData[i].Name);
+                    certCounts.push(certData[i].Number_of_Certified_Professionals__c);
+                }
+                const config = {
+                    type: CHART_CONFIG.type,
+                    data: {
+                        labels: certLabels,
+                        datasets: [{
+                            backgroundColor: CHART_CONFIG.color,
+                            data: certCounts
+                        }]
+                    },
+                    options: CHART_CONFIG.options
+                };
+                const ctx = this.template
+                    .querySelector(CHART_CONFIG.dom_selector)
+                    .getContext('2d');
+                this._chart = new window.Chart(ctx, config);
             })
             .catch(error => {
                 this.error = error;
